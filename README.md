43f1d097d447c10fdc8c73edd81ef8f232050118
hisava <veretennikovalexey@gmail.com>
2023-11-19 19:37:13 +0000

    alex

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 1. ~~Скачиваем VIRTUALBOX~~
 2. ~~Скачиваем VMWARE~~
 3. Скачиваем UTM
+
+./sum < input.txt
+
---
+++ sum.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num1, num2;
+  cin >> num1 >> num2;
+  cout << "The sum is " << num1 + num2 << endl;
+  return 0;
+}
